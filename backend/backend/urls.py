61c927669d8ee5100793913de238dcdcf262f200
PashkovichKaterina <pashkovichkate@gmail.com>
2022-02-01 12:41:48 +0300

    Add Swagger for generate API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,22 +1,15 @@
-"""backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""backend URL Configuration"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("simpleMusicService.urls")),
+
+    path("openapi/", get_schema_view(title="Simple music service"), name="openapi_schema"),
+    path("docs/", TemplateView.as_view(template_name="documentation.html",
+                                       extra_context={"schema_url": "openapi_schema"}),
+         name="swagger-ui"),
 ]
