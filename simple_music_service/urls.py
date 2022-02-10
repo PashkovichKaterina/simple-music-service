92cc398b9d39d095de34824a5a6b17f6760ce3af
PashkovichKaterina <pashkovichkate@gmail.com>
2022-02-10 11:30:16 +0300

    Update view for Signup

--- simple_music_service/urls.py
+++ simple_music_service/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import SongViewSet, ArtistViewSet, MyTokenObtainPairView, SignupAPIView
+from .views import SongViewSet, ArtistViewSet, MyTokenObtainPairView, SignupViewSet
 
 router = routers.DefaultRouter()
 router.register(r"songs", SongViewSet)
 router.register(r"artists", ArtistViewSet)
+router.register(r"signup", SignupViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("signup/", SignupAPIView.as_view()),
     path("token/", MyTokenObtainPairView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view()),
 ]
--- simple_music_service/views.py
+++ simple_music_service/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, generics
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
@@ -26,15 +25,8 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-class SignupAPIView(generics.GenericAPIView):
+class SignupViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ["post"]
     permission_classes = (AllowAny,)
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response(
-            UserSerializer(user, context=self.get_serializer_context()).data
-        )
